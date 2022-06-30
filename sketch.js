e0a190c5ba320c4fa8b9ab9d162c409a03214b45
TainaraLongaretti <106778351+TainaraLongaretti@users.noreply.github.com>
2022-06-30 19:18:11 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,79 +1,61 @@
-var garden,rabbit;
-var gardenImg,rabbitImg;
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
 
 function preload(){
-  gardenImg = loadImage("garden.png");
-  rabbitImg = loadImage("rabbit.png");
-  appleImg = loadImage("apple.png");
- orangeImg = loadImage("orangeLeaf.png");
-  redImg = loadImage("redImage.png"); 
+  //loadImage (carregarImagem) da pista)
+  pathImg=loadImage("path.png");
+  //loadAnimation (carregarAnimação) de corrida para o menino
+  boyImg=loadAnimation("Jake1.png", "Jake2.png", "jake3.png", "jake4.PNG", "jake5.png");
+ 
 }
 
 function setup(){
   
   createCanvas(400,400);
+ //crie um sprite para a pista 
+ path=createSprite(200, 100, 100, 50);
+//adicione uma imagem para a pista
+path.addImage(pathImg);
+//Faça com que a pista seja um fundo que se move dando a ela velocity Y.
+path.velocityY=-5;
+path.scale=1.2;
+
+//crie um sprite de menino
+boy=createSprite(200, 300, 100, 50);
+//adicione uma animação de corrida para ele
+boy.addAnimation("boy", boyImg);
+boy.scale=0.8;
   
-// mover o fundo
-garden=createSprite(200,200);
-garden.addImage(gardenImg);
+//crie um limite à esquerda
+leftBoundary=createSprite(0,0,100,800);
+//defina visibilidade como falsa para o limite à esquerda
+leftBoundary.visible=false;
 
-//criar sprite do coelho
-rabbit = createSprite(180,340,30,30);
-rabbit.scale =0.09;
-rabbit.addImage(rabbitImg);
+//crie um limite à direita
+rightBoundary=createSprite(410,0,100,800);
+//defina visibilidade como falsa para o limite à direita
+rightBoundary.visible=false;
 }
-
 
 function draw() {
   background(0);
-
-  edges= createEdgeSprites();
-  rabbit.collide(edges);
-
-  drawSprites();
-  function createApples(){
-    //if (frameCount % 80 == 0){
-    apple=createSprite(random(50, 350), 40, 10, 10);
-    apple.addImage(appleImg);
-    apple.scale=0.07;
-    apple.velocityY=3;
-    apple.lifetime=150;
-  }//}
-
-  function createOrange(){
-   // if (frameCount % 80 == 0){
-    orangeL=createSprite(random(50, 350), 40, 10, 10);
-    orangeL.addImage(orangeImg);
-    orangeL.scale=0,08;
-    orangeL.velocityY=3;
-    orangeL.lifetime=150;
-  }//}
-
-  function createRed(){
-    //if (frameCount % 80 == 0){
-    redL=createSprite(random(50, 350), 40, 10, 10);
-    redL.addImage(redImg);
-    redL.scale=0.06;
-    redL.velocityY=3;
-    redL.lifetime=150;
-  }//}
-
-  var select_sprites=Math.round(random(1,3));
-  //criar sprites aleatóriamente
-  if (frameCount % 60 == 0){
-    if (select_sprites == 1){
-      createApples();
-      console.log(frameCount);
-    } 
-    else if (select_sprites == 2){
-      createOrange();
-      console.log(frameCount);
-    }
-    else {
-      createRed();
-    }
-    
-  }
-  rabbit.x=World.mouseX;
+  path.velocityY = 4;
   
+  // mover o menino com o mouse usando mouseX
+  boy.x=World.mouseX;
+  
+  edges= createEdgeSprites();
+  boy.collide(edges[3]);
+  // colidir o menino com os limites invisíveis da esquerda e da direita
+  boy.collide(leftBoundary, rightBoundary);
+  
+  //código para redefinir o fundo
+  if(path.y > 500 ){
+    path.y = height/2;
+  }
+
+
+  
+  drawSprites();
 }
