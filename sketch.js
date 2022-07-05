663c1d21ee7b90d7e5e273b3572f8e8134f31fa4
TainaraLongaretti <106778351+TainaraLongaretti@users.noreply.github.com>
2022-07-05 08:26:16 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,23 +1,12 @@
-
-var play, end; //Declare a variável para PLAY e END
-var play=1;
-var end=0; //inicialize o valor para a variável
-var gameState=play; //Atribua o valor de gameState como PLAY
-
-var bow , arrow,  background, redB, pinkB, greenB ,blueB ,arrowGroup;
+var bow , arrow,  background;
 var bowImage, arrowImage, green_balloonImage, red_balloonImage, pink_balloonImage ,blue_balloonImage, backgroundImage;
 
-var score =0;
 function preload(){
   
   backgroundImage = loadImage("background0.png");
-  
   arrowImage = loadImage("arrow0.png");
   bowImage = loadImage("bow0.png");
   red_balloonImage = loadImage("red_balloon0.png");
-  green_balloonImage = loadImage("green_balloon0.png");
-  pink_balloonImage = loadImage("pink_balloon0.png");
-  blue_balloonImage = loadImage("blue_balloon0.png");
   
 }
 
@@ -26,115 +15,62 @@ function preload(){
 function setup() {
   createCanvas(400, 400);
   
-  //crie o fundo
+  //criar um plano de fundo
   scene = createSprite(0,0,400,400);
   scene.addImage(backgroundImage);
   scene.scale = 2.5
   
-  // criando arco para a flecha
+  // criar um arco para a flecha
   bow = createSprite(380,220,20,50);
   bow.addImage(bowImage); 
   bow.scale = 1;
-  
-   score = 0  
- redB= new Group();
- 
-  arrowGroup= new Group();
-
   
 }
 
 function draw() {
  background(0);
+ redBalloon();
+ blueBalloon();
+ greenBalloon();
+ pinkBalloon();
 
-   if (gameState==play){//Adicione a condição para gameState = PLAY
-    scene.velocityX = -3 
+  // mover o chão
+    scene.velocityX = -3;
 
     if (scene.x < 0){
       scene.x = scene.width/2;
     }
   
-  //arco em movimento
+  //mover o arco
   bow.y = World.mouseY
   
-   // soltar arco quando a tecla espaço for pressionada
+   // soltar a flecha quando a tecla espaço for pressionada
   if (keyDown("space")) {
     createArrow();
     
-  }}
+  }
   
-  //criando inimigos continuamente
+  //mude o valor do balão aleatório para 4
   var select_balloon = Math.round(random(1,4));
   
   if (World.frameCount % 100 == 0) {
-    switch(select_balloon ){
-      case 1: redBalloon();
-      break;
-      case 2:blueBalloon();
-      break;
-      case 3:pinkBalloon();
-      break;
-      case 4:greenBalloon();
-      break;
-      default:break;
-    }
-  }
+   switch(select_balloon){
+    case 1: redBalloon.addImage(red_balloonImage);
+    break;
+    case 2: blueBalloon.addImage(blue_balloonImage);
+    break;
+    case 3: greenBalloon.addImage(green_balloonImage);
+    break;
+    case 4: pinkBalloon.addImage(pink_balloonImage);
+    break;
 
-  if (arrowGroup.isTouching(redB)) {
-    redB.destroyEach();
-
-    gameState=end; 
-
-
-}
-   else if (gameState==end){//escreva uma condição para o estado END
-   bow.destroyEach();//Adicione o código para destruir o arco
-   scene.velocityX=0;//defina a velocidade do fundo como 0
- 
-   }
-
+  }}
+  
   drawSprites();
-  textSize(10);
-  fill("black");
-  text("score: "+score, 180, 25);//Adicione a condição de texto para exibir a pontuação.
 }
 
 
-function redBalloon() {
-  var red = createSprite(0,Math.round(random(20, 370)), 10, 10);
-  red.addImage(red_balloonImage);
-  red.velocityX = 3;
-  red.lifetime = 150;
-  red.scale = 0.1;
-  redB.add(red);
-}
-
-function blueBalloon() {
-  var blue = createSprite(0,Math.round(random(20, 370)), 10, 10);
-  blue.addImage(blue_balloonImage);
-  blue.velocityX = 3;
-  blue.lifetime = 150;
-  blue.scale = 0.1;
-}
-
-function greenBalloon() {
-  var green = createSprite(0,Math.round(random(20, 370)), 10, 10);
-  green.addImage(green_balloonImage);
-  green.velocityX = 3;
-  green.lifetime = 150;
-  green.scale = 0.1;
-}
-
-function pinkBalloon() {
-  var pink = createSprite(0,Math.round(random(20, 370)), 10, 10);
-  pink.addImage(pink_balloonImage);
-  pink.velocityX = 3;
-  pink.lifetime = 150;
-  pink.scale = 1
-
-}
-
-// Criar flechas para o arco
+// criar flechas para o arco
  function createArrow() {
   var arrow= createSprite(100, 100, 60, 10);
   arrow.addImage(arrowImage);
@@ -143,6 +79,37 @@ function pinkBalloon() {
   arrow.velocityX = -4;
   arrow.lifetime = 100;
   arrow.scale = 0.3;
-  arrowGroup.add(arrow);
+}
 
+
+function redBalloon() {
+  var red = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  red.addImage(red_balloonImage);
+  red.velocityX = 3;
+  red.scale = 0.1;
+  red.lifetime = 150;
+}
+
+function blueBalloon() {
+  var blue = createSprite(0, Math.round(random(20, 370)), 10, 10); //crieSprite para o balão 
+  blue.addImage(blue_balloonImage); //adicioneImagem para o balão
+  blue.velocityX = 3; //adicione velocidade para fazer o balão se mover
+   blue.scale = 0.1; //mudar a dimensão do balão
+  blue.lifetime = 150; //atribua tempo de vida ao balão
+}
+
+function greenBalloon() {
+  var green = createSprite(0, Math.round(random(20, 370)), 10, 10); //crieSprite para o balão
+  green.addImage(green_balloonImage); //adicioneImagem para o balão
+  green.velocityX = 3; //adicione velocidade para fazer o balão se mover
+  green.scale = 0.1; //mudar a dimensão do balão
+  green.lifetime = 150; //atribua tempo de vida ao balão
+}
+
+function pinkBalloon() {
+  var pink = createSprite(0, Math.round(random(20, 370)), 10, 10); //crieSprite para o balão
+  pink.addImage(pink_balloonImage); //adicioneImagem para o balão
+  pink.velocityX = 3; //adicione velocidade para fazer o balão se mover
+  pink.scale = 0.1; //mudar a dimensão do balão
+  pink.lifetime = 150; //atribua tempo de vida ao balão
 }
